97379dc46b2a312642bc35be37fc873505ee03c6
Jburg1414 <Jburgess1414@gmail.com>
2022-01-12 19:20:26 -0700

    length prompt created

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,32 @@
 // Assignment code here
 
 
+
+// When button is clicked prompted to generate password
+// Prompt criteria asks for length of password 8-128 characters
+// Prompt asks for uppercase, lowercase, numeric, and special
+// validated and one character type selected
+// All prompts answered then password is generated and displayed in alert
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var promptLength = prompt("Select a length for your new password between 8-128 characters.")
+    if (promptLength >=8 && promptLength <= 128) {
+    alert("You have selected a password length of " + promptLength + " characters.");
+
+    localStorage.setItem("length", promptLength)
+  } else {
+    alert("Please enter a value between 8-128!");
+  } 
+
+  var 
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
