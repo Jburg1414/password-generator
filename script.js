cf0b0fffd95aa3a8429657878dda6ca489beaea3
Jburg1414 <Jburgess1414@gmail.com>
2022-01-14 19:52:00 -0700

    restructured files

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createNewPassword() {
   }
 
   if (usedCharacterType.includeSpecial) {
-    pickedOtherCharacters += " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    pickedOtherCharacters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   }
 
   for (var i = 0; i < passwordLength; i ++) {
