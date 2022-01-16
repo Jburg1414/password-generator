333d6494cf8804c85d1ece04458846a20172b879
Jburg1414 <Jburgess1414@gmail.com>
2022-01-16 14:25:30 -0700

    added comments all along code to describe what is going on through out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 // Assignment code here
 
+// When button is clicked prompted to generate password
+
+// this function is to create the random password
 function createNewPassword() {
+  // start with variable createNewPassword set equal to a blank string
   var createNewPassword = "";
+  // variable passwordLength set equal to the function retrievePasswordLength which is defined below
   var passwordLength = retrievePasswordLength();
+  // variable usedCharacterType set equal to the function retrieveCharacterType which is defined below
   var usedCharacterType = retrieveCharacterType();
+  // variable pickedOtherCharacters set equal to a blank string
   var pickedOtherCharacters = "";
   
+  // if statements for the different character types, depending what is selected will determine what types of characters to include in the randomized characters
   if (usedCharacterType.includeLowercase) {
     pickedOtherCharacters += "abcdefghijklmnopqrstuvwxyz";
   }
@@ -22,10 +30,13 @@ function createNewPassword() {
     pickedOtherCharacters += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   }
   
+  // for loop to randomly select characters based on what character sets were selected and how long the password was selected to be. Each character is added behind the previous character to create the random password.
   for (var i = 0; i < passwordLength; i ++) {
     createNewPassword += pickedOtherCharacters.charAt(Math.floor(Math.random() * pickedOtherCharacters.length));
   }
   
+
+  // return the randomly created password 
   return createNewPassword;
 }  
 
@@ -33,16 +44,18 @@ function createNewPassword() {
 function retrievePasswordLength(valueEntered) {
   var criteriaLength = "Select a password length between 8-128 characters.";
   
-  // 
+  // prompt value if password length is outside of 8-128 range
   if (valueEntered) {
     criteriaLength = valueEntered + " is not between 8-128. " + criteriaLength;
   }
-  // add null if statement 
-  // when null if statement add remove const defaultLeng
+  // constant default length of passwords 
   const defaultLength = 8;
 
-  var length = Number(prompt(criteriaLength, defaultLength));
+  // password length 
+  var length = Number(prompt(criteriaLength));
 
+
+  // if var length is not a number return default length, else if the length is less than 8 or greater than 128 prompt, else return the selected password length
   if (isNaN(length)) {
     return defaultLength;
   } else if (length < 8 || length > 128) {
@@ -52,6 +65,7 @@ function retrievePasswordLength(valueEntered) {
   }
 }
 
+// this prompts the employee to select what characters to include in the password 
 function retrieveCharacterType () {
   var selectedPasswordCharacters = {
     includeLowercase: confirm("Include lowercase characters?"),
@@ -59,32 +73,29 @@ function retrieveCharacterType () {
     includeNumeric: confirm("Include numeric characters?"),
     includeSpecial: confirm("Include special characters?")
   };
-  // change selectedOptionsDictionary to selectedPasswordCharacters
- if (!selectedOptionsDictionary.includeLowercase && !selectedOptionsDictionary.includeUppercase && !selectedOptionsDictionary.includeNumeric && !selectedOptionsDictionary.includeSpecial) {
-   alert ("You have not selected any character options. Please select at least one.");
+  
+  // if statement to prompt the employee that no characters have been chosen and top select character sets, else return the selected character types  
+ if (!selectedPasswordCharacters.includeLowercase && !selectedPasswordCharacters.includeUppercase && !selectedPasswordCharacters.includeNumeric && !selectedPasswordCharacters.includeSpecial) {
+   alert("You have not selected any character options. Please select at least one!");
    return retrieveCharacterType();
  } else {
     return selectedPasswordCharacters;
  }
 }
 
-
-// When button is clicked prompted to generate password
-// Prompt asks for uppercase, lowercase, numeric, and special
-// validated and one character type selected
-// All prompts answered then password is generated and displayed in alert
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-
- var password = createNewPassword();
- var passwordText = document.querySelector("#password");
- 
- passwordText.value = password
+  
+  // this variable calls out the function to create the new password 
+  var password = createNewPassword();
+  // this variable calls out where the generated password will be displayed on the webpage
+  var passwordText = document.querySelector("#password");
+  
+  // this sets the passwordText to the value of the variable password
+  passwordText.value = password
 }
 
 
